8915bbab7da8a6eb54a284384d35088a01ee535c
Matt Rudge <matt.rudge@codeinstitute.net>
2021-08-17 13:17:54 +0000

    Deployment commit

---
+++ controllers/default.js
@@ -0,0 +1,65 @@
+const Pty = require('node-pty');
+const fs = require('fs');
+
+exports.install = function () {
+
+    // Route to views/index
+    ROUTE('/');
+
+    // WebSocket route
+    WEBSOCKET('/', socket, ['raw']);
+
+};
+
+function socket() {
+
+    var self = this;
+
+    self.encodedecode = false;
+    self.autodestroy();
+
+    self.on('open', function (client) {
+
+        // Each client will have own terminal
+        client.tty = Pty.spawn('python3', ['run.py'], {
+            name: 'xterm-color',
+            cols: 80,
+            rows: 24,
+            cwd: process.env.PWD,
+            env: process.env
+        });
+
+        client.tty.on('exit', function (code, signal) {
+            // What now?
+            client.tty = null;
+            client.close();
+            console.log("Process killed");
+        });
+
+        client.tty.on('data', function (data) {
+            client.send(data);
+        });
+
+    });
+
+    self.on('close', function (client) {
+        if (client.tty) {
+            client.tty.kill(9);
+            client.tty = null;
+            console.log("Process killed and terminal unloaded");
+        }
+    });
+
+    self.on('message', function (client, msg) {
+        client.tty && client.tty.write(msg);
+    });
+}
+
+if (process.env.CREDS != null) {
+    fs.writeFile('creds.json', process.env.CREDS, 'utf8', function (err) {
+        if (err) {
+            console.log('Error writing file: ', err);
+            socket.emit("console_output", "Error saving credentials: " + err);
+        }
+    });
+}
