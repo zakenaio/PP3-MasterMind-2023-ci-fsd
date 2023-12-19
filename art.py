a9dd020f2046522cd73da251bca8a3d0e47410fe
Anders Miller <anders.miller@gmail.com>
2023-12-19 14:04:42 +0100

    Initial commit, add of art

---
+++ art.py
@@ -0,0 +1,18 @@
+logo = """
+8888ba.88ba                      dP                     8888ba.88ba  oo                dP 
+88  `8b  `8b                     88                     88  `8b  `8b                   88 
+88   88   88 .d8888b. .d8888b. d8888P .d8888b. 88d888b. 88   88   88 dP 88d888b. .d888b88 
+88   88   88 88'  `88 Y8ooooo.   88   88ooood8 88'  `88 88   88   88 88 88'  `88 88'  `88 
+88   88   88 88.  .88       88   88   88.  ... 88       88   88   88 88 88    88 88.  .88 
+dP   dP   dP `88888P8 `88888P'   dP   `88888P' dP       dP   dP   dP dP dP    dP `88888P8 
+Extended Python Version
+"""
+
+win = """
+ooooo  oooo ooooooo  ooooo  oooo      oooo     oooo ooooo oooo   oooo 
+  888  88 o888   888o 888    88        88   88  88   888   8888o  88  
+    888   888     888 888    88         88 888 88    888   88 888o88  
+    888   888o   o888 888    88          888 888     888   88   8888  
+   o888o    88ooo88    888oo88            8   8     o888o o88o    88  
+                                                                      
+"""
