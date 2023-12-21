8aa2bb8ff472223f6e906dde141cd16295e23724
Anders Miller <anders.miller@gmail.com>
2023-12-21 08:15:43 +0100

    Add generate code - print to terminal for test

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 from art import logo, win
-
+import random
 print(logo)
+
+COLORS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 5
+CODE_LENGTH = 4
+
+def generate_code():
+    code = []
+    for _ in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+        code.append(color)
+    print(code)
+    return code
+
+def game():
+    generate_code()
+    
+
+game()
