3c72fbfa09973e93747108dcbc277a82f7d01dd2
Anders Miller <anders.miller@gmail.com>
2023-12-21 08:27:56 +0100

    Add Player Guess

--- run.py
+++ run.py
@@ -7,6 +7,12 @@ TRIES = 5
 CODE_LENGTH = 4
 
 def generate_code():
+    """
+    Generates a random code by selecting a color 
+    from the COLORS list for each position in the
+    code and then returning the code.
+    
+    """
     code = []
     for _ in range(CODE_LENGTH):
         color = random.choice(COLORS)
@@ -14,8 +20,27 @@ def generate_code():
     print(code)
     return code
 
+
+def guess_code():
+    while True:
+        guess = input("Guess: ").upper().split(" ")
+
+        if len(guess) != CODE_LENGHT:
+            print(f"You must guess {CODE_LENGHT} colors.")
+            continue
+
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid colors: {color}. Try again.")
+                break
+        else:
+            break
+
+    return guess
+
 def game():
     generate_code()
+    guess_code()
     
 
 game()
